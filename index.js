07248204596ea3a7dbe38b4bf111ddfa523235ed
afrique18 <vincentv1225@gmail.com>
2023-05-19 22:50:47 -0400

    Add function to prompt user for infomation

--- index.js
+++ index.js
@@ -39,3 +39,54 @@ If you have any questions or need further assistance, feel free to reach out.
 `;
 }
 
+// Function to prompt the user for information
+function promptUser() {
+    return inquirer.prompt([
+        {
+           type: 'input',
+           name: 'title',
+           message: 'Enter the title of your project:',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Enter a description of your project:', 
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Enter installation instructions:', 
+        },
+        {
+            type: 'input',
+            name: 'usuage',
+            message: 'Enter usuage information:', 
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Enter contribution guidelines:', 
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Enter test instructions:', 
+        },
+        {
+            type: 'input',
+            name: 'license',
+            message: 'Choose a license for your application:', 
+            choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', 'None'],
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub usuername:', 
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email address:', 
+        },
+    ]);
+}
